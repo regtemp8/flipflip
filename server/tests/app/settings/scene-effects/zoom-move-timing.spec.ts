6e5c0eae244afdd21bf1cbc0bdffa33d6838238c
regtemp8 <regulartemporary8@proton.me>
2024-10-28 20:14:31 +0200

    Split scene effect settings tests

---
+++ server/tests/app/settings/scene-effects/zoom-move-timing.spec.ts
@@ -0,0 +1,250 @@
+import { test, expect } from '@playwright/test'
+import {VTF} from 'flipflip-common'
+import {changeSlider} from '../../utils'
+
+test.use({ storageState: 'server/tests/session.json' })
+test.beforeEach(async ({ page }) => {
+  await page.goto('/settings/scene-effects')
+})
+
+test('Enable zoom/move timing', async ({ page }) => {
+    const container = await page.locator('.MuiGrid2-container .MuiGrid2-root > .MuiCollapse-entered:has-text("Timing")')
+    await expect(container).not.toBeVisible()
+    await expect(page.getByLabel('Zoom', { exact: true })).not.toBeChecked()
+    await expect(page.getByText('None').first()).toBeVisible()
+    await expect(page.getByText('None').nth(1)).toBeVisible()
+
+    await page.getByLabel('Zoom', { exact: true }).check();
+    await expect(container).toBeVisible()
+
+    await page.getByLabel('Zoom', { exact: true }).uncheck();
+    await expect(container).not.toBeVisible()
+
+    await page.getByText('None').first().click()
+    await page.getByRole('option', { name: 'Left', exact: true }).click();
+    await expect(container).toBeVisible()
+
+    await page.getByText('Left').first().click()
+    await page.getByRole('option', { name: 'Right', exact: true }).click();
+    await expect(container).toBeVisible()
+
+    await page.getByText('Right').first().click()
+    await page.getByRole('option', { name: 'Left/Right', exact: true }).click();
+    await expect(container).toBeVisible()
+
+    await page.getByText('Left/Right').first().click()
+    await page.getByRole('option', { name: 'None', exact: true }).click();
+    await expect(container).not.toBeVisible()
+
+    await page.getByText('None').nth(1).click()
+    await page.getByRole('option', { name: 'Up', exact: true }).click();
+    await expect(container).toBeVisible()
+
+    await page.getByText('Up').first().click()
+    await page.getByRole('option', { name: 'Down', exact: true }).click();
+    await expect(container).toBeVisible()
+
+    await page.getByText('Down').first().click()
+    await page.getByRole('option', { name: 'Up/Down', exact: true }).click();
+    await expect(container).toBeVisible()
+
+    const responsePromise = page.waitForResponse((res) => {
+        const request = res.request()
+        return new URL(request.url()).pathname === '/api/scenes/1' 
+            && request.method() === 'PATCH' 
+            && request.postDataJSON()?.vertTransType === VTF.none
+            && res.status() === 204
+    })
+    await page.getByText('Up/Down').first().click()
+    await page.getByRole('option', { name: 'None', exact: true }).click();
+    await expect(container).not.toBeVisible()
+    await responsePromise
+})
+
+test('Constant zoom/move timing', async ({ page }) => {
+    await page.getByLabel('Zoom', { exact: true }).check();
+    await expect(page.getByLabel('Zoom', { exact: true })).toBeChecked()
+    const container = await page.locator('.MuiGrid2-container .MuiGrid2-root > .MuiCollapse-entered:has-text("Timing")')
+
+    await page.getByRole('combobox').nth(2).click();
+    await page.getByRole('option', { name: 'Constant', exact: true }).click();
+    await expect(container.locator('.MuiCollapse-entered').getByRole('spinbutton', { name: 'For' })).toBeVisible()
+    await expect(container.locator('.MuiCollapse-entered').getByRole('spinbutton', { name: 'For' })).toHaveAttribute('type', 'number')
+    await expect(container.locator('.MuiCollapse-entered').getByRole('spinbutton', { name: 'For' })).toHaveAttribute('min', '0')
+    await expect(container.locator('.MuiCollapse-entered').getByRole('spinbutton', { name: 'For' })).toHaveAttribute('step', '100')
+
+    await expect(container.locator('.MuiCollapse-entered').getByRole('spinbutton', { name: 'Between' })).not.toBeVisible()
+    await expect(container.locator('.MuiCollapse-entered').getByRole('spinbutton', { name: 'and' })).not.toBeVisible()
+    await expect(container.locator('.MuiCollapse-entered .MuiTypography-caption')).not.toBeVisible();
+    await expect(container.locator('.MuiCollapse-entered .MuiSlider-root')).not.toBeVisible();
+
+    const responsePromise = page.waitForResponse((res) => {
+        const request = res.request()
+        return new URL(request.url()).pathname === '/api/scenes/1' 
+            && request.method() === 'PATCH' 
+            && request.postDataJSON()?.zoom === false
+            && res.status() === 204
+    })
+    await page.getByLabel('Zoom', { exact: true }).uncheck();
+    await expect(page.getByLabel('Zoom', { exact: true })).not.toBeChecked()
+    await responsePromise
+})
+
+test('Random zoom/move timing', async ({ page }) => {
+    await page.getByLabel('Zoom', { exact: true }).check();
+    await expect(page.getByLabel('Zoom', { exact: true })).toBeChecked()
+    const container = await page.locator('.MuiGrid2-container .MuiGrid2-root > .MuiCollapse-entered:has-text("Timing")')
+
+    await page.getByRole('combobox').nth(2).click();
+    await page.getByRole('option', { name: 'Random', exact: true }).click();
+    await expect(container.locator('.MuiCollapse-entered').getByRole('spinbutton', { name: 'Between' })).toBeVisible()
+    await expect(container.locator('.MuiCollapse-entered').getByRole('spinbutton', { name: 'Between' })).toHaveAttribute('type', 'number')
+    await expect(container.locator('.MuiCollapse-entered').getByRole('spinbutton', { name: 'Between' })).toHaveAttribute('min', '0')
+    await expect(container.locator('.MuiCollapse-entered').getByRole('spinbutton', { name: 'Between' })).toHaveAttribute('step', '100')
+    await expect(container.locator('.MuiCollapse-entered').getByRole('spinbutton', { name: 'and' })).toBeVisible()
+    await expect(container.locator('.MuiCollapse-entered').getByRole('spinbutton', { name: 'and' })).toHaveAttribute('type', 'number')
+    await expect(container.locator('.MuiCollapse-entered').getByRole('spinbutton', { name: 'and' })).toHaveAttribute('min', '0')
+    await expect(container.locator('.MuiCollapse-entered').getByRole('spinbutton', { name: 'and' })).toHaveAttribute('step', '100')
+
+    await expect(container.locator('.MuiCollapse-entered').getByRole('spinbutton', { name: 'For' })).not.toBeVisible()
+    await expect(container.locator('.MuiCollapse-entered .MuiTypography-caption')).not.toBeVisible();
+    await expect(container.locator('.MuiCollapse-entered .MuiSlider-root')).not.toBeVisible();
+
+    const responsePromise = page.waitForResponse((res) => {
+        const request = res.request()
+        return new URL(request.url()).pathname === '/api/scenes/1' 
+            && request.method() === 'PATCH' 
+            && request.postDataJSON()?.zoom === false
+            && res.status() === 204
+    })
+    await page.getByLabel('Zoom', { exact: true }).uncheck();
+    await expect(page.getByLabel('Zoom', { exact: true })).not.toBeChecked()
+    await responsePromise
+})
+
+test('Wave zoom/move timing', async ({ page }) => {
+    await page.getByLabel('Zoom', { exact: true }).check();
+    await expect(page.getByLabel('Zoom', { exact: true })).toBeChecked()
+    const container = await page.locator('.MuiGrid2-container .MuiGrid2-root > .MuiCollapse-entered:has-text("Timing")')
+
+    await page.getByRole('combobox').nth(2).click();
+    await page.getByRole('option', { name: 'Wave', exact: true }).click();
+    await expect(container.locator('.MuiCollapse-entered').getByRole('spinbutton', { name: 'Between' })).toBeVisible()
+    await expect(container.locator('.MuiCollapse-entered').getByRole('spinbutton', { name: 'Between' })).toHaveAttribute('type', 'number')
+    await expect(container.locator('.MuiCollapse-entered').getByRole('spinbutton', { name: 'Between' })).toHaveAttribute('min', '0')
+    await expect(container.locator('.MuiCollapse-entered').getByRole('spinbutton', { name: 'Between' })).toHaveAttribute('step', '100')
+    await expect(container.locator('.MuiCollapse-entered').getByRole('spinbutton', { name: 'and' })).toBeVisible()
+    await expect(container.locator('.MuiCollapse-entered').getByRole('spinbutton', { name: 'and' })).toHaveAttribute('type', 'number')
+    await expect(container.locator('.MuiCollapse-entered').getByRole('spinbutton', { name: 'and' })).toHaveAttribute('min', '0')
+    await expect(container.locator('.MuiCollapse-entered').getByRole('spinbutton', { name: 'and' })).toHaveAttribute('step', '100')
+    await expect(container.locator('.MuiCollapse-entered .MuiTypography-caption')).toHaveText('Wave Rate');
+    const slider = container.locator('.MuiCollapse-entered .MuiSlider-root')
+    await expect(slider).toBeVisible();
+    const thumb = await slider.locator('.MuiSlider-thumb')
+    await thumb.hover()
+    await expect(thumb.locator('.MuiSlider-valueLabelOpen')).toHaveText('100')
+
+    await changeSlider(page, thumb, slider, 0)
+    await thumb.hover()
+    await expect(thumb.locator('.MuiSlider-valueLabelOpen')).toHaveText('1')
+
+    await changeSlider(page, thumb, slider, 0.01)
+    await thumb.hover()
+    await expect(thumb.locator('.MuiSlider-valueLabelOpen')).toHaveText('2')
+
+    await changeSlider(page, thumb, slider, 0.99)
+    await thumb.hover()
+    await expect(thumb.locator('.MuiSlider-valueLabelOpen')).toHaveText('99')
+
+    await changeSlider(page, thumb, slider, 1)
+    await thumb.hover()
+    await expect(thumb.locator('.MuiSlider-valueLabelOpen')).toHaveText('100')
+
+    await expect(container.locator('.MuiCollapse-entered').getByRole('spinbutton', { name: 'For' })).not.toBeVisible()
+
+    const responsePromise = page.waitForResponse((res) => {
+        const request = res.request()
+        return new URL(request.url()).pathname === '/api/scenes/1' 
+            && request.method() === 'PATCH' 
+            && request.postDataJSON()?.zoom === false
+            && res.status() === 204
+    })
+    await page.getByLabel('Zoom', { exact: true }).uncheck();
+    await expect(page.getByLabel('Zoom', { exact: true })).not.toBeChecked()
+    await responsePromise
+})
+
+test('Audio BPM zoom/move timing', async ({page}) => {
+    await page.getByLabel('Zoom', { exact: true }).check();
+    await expect(page.getByLabel('Zoom', { exact: true })).toBeChecked()
+    const container = await page.locator('.MuiGrid2-container .MuiGrid2-root > .MuiCollapse-entered:has-text("Timing")')
+
+    await page.getByRole('combobox').nth(2).click();
+    await page.getByRole('option', { name: 'Audio BPM', exact: true }).click();
+    const slider = container.locator('.MuiCollapse-entered .MuiSlider-root')
+    await expect(slider).toBeVisible();
+    await expect(container.locator('.MuiCollapse-entered').getByRole('spinbutton', { name: 'For' })).not.toBeVisible()
+    await expect(container.locator('.MuiCollapse-entered').getByRole('spinbutton', { name: 'Between' })).not.toBeVisible()
+    await expect(container.locator('.MuiCollapse-entered').getByRole('spinbutton', { name: 'and' })).not.toBeVisible()
+
+    const thumb = await slider.locator('.MuiSlider-thumb')
+    await thumb.hover()
+    await expect(thumb.locator('.MuiSlider-valueLabelOpen')).toHaveText('1x')
+    await expect(container.locator('.MuiCollapse-entered .MuiTypography-caption')).toHaveText('BPM Multiplier 1x');
+
+    await changeSlider(page, thumb, slider, 0)
+    await thumb.hover()
+    await expect(thumb.locator('.MuiSlider-valueLabelOpen')).toHaveText('0.1x')
+    await expect(container.locator('.MuiCollapse-entered .MuiTypography-caption')).toHaveText('BPM Multiplier 0.1x');
+
+    await changeSlider(page, thumb, slider, 0.01)
+    await thumb.hover()
+    await expect(thumb.locator('.MuiSlider-valueLabelOpen')).toHaveText('0.2x')
+    await expect(container.locator('.MuiCollapse-entered .MuiTypography-caption')).toHaveText('BPM Multiplier 0.2x');
+
+    await changeSlider(page, thumb, slider, 0.99)
+    await thumb.hover()
+    await expect(thumb.locator('.MuiSlider-valueLabelOpen')).toHaveText('9.9x')
+    await expect(container.locator('.MuiCollapse-entered .MuiTypography-caption')).toHaveText('BPM Multiplier 9.9x');
+
+    await changeSlider(page, thumb, slider, 1)
+    await thumb.hover()
+    await expect(thumb.locator('.MuiSlider-valueLabelOpen')).toHaveText('10x')
+    await expect(container.locator('.MuiCollapse-entered .MuiTypography-caption')).toHaveText('BPM Multiplier 10x');
+
+    const responsePromise = page.waitForResponse((res) => {
+        const request = res.request()
+        return new URL(request.url()).pathname === '/api/scenes/1' 
+            && request.method() === 'PATCH' 
+            && request.postDataJSON()?.zoom === false
+            && res.status() === 204
+    })
+    await page.getByLabel('Zoom', { exact: true }).uncheck();
+    await expect(page.getByLabel('Zoom', { exact: true })).not.toBeChecked()
+    await responsePromise
+})
+
+test('With scene zoom/move timing', async ({ page }) => {
+    await page.getByLabel('Zoom', { exact: true }).check();
+    await expect(page.getByLabel('Zoom', { exact: true })).toBeChecked()
+    const container = await page.locator('.MuiGrid2-container .MuiGrid2-root > .MuiCollapse-entered:has-text("Timing")')
+
+    await page.getByRole('combobox').nth(2).click();
+    await page.getByRole('option', { name: 'With Scene', exact: true }).click();
+    await expect(container.locator('.MuiCollapse-entered').getByRole('spinbutton', { name: 'For' })).not.toBeVisible()
+    await expect(container.locator('.MuiCollapse-entered').getByRole('spinbutton', { name: 'Between' })).not.toBeVisible()
+    await expect(container.locator('.MuiCollapse-entered').getByRole('spinbutton', { name: 'and' })).not.toBeVisible()
+    await expect(container.locator('.MuiCollapse-entered .MuiTypography-root')).not.toBeVisible()
+    await expect(container.locator('.MuiCollapse-entered .MuiSlider-root')).not.toBeVisible()
+
+    const responsePromise = page.waitForResponse((res) => {
+        const request = res.request()
+        return new URL(request.url()).pathname === '/api/scenes/1' 
+            && request.method() === 'PATCH' 
+            && request.postDataJSON()?.zoom === false
+            && res.status() === 204
+    })
+    await page.getByLabel('Zoom', { exact: true }).uncheck();
+    await expect(page.getByLabel('Zoom', { exact: true })).not.toBeChecked()
+    await responsePromise
+})
