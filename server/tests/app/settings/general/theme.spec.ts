aaff8cfd31db6a42b6759ff42ae576b3941b1f13
regtemp8 <regulartemporary8@proton.me>
2024-11-15 00:27:51 +0200

    Theme pick colors test

--- server/tests/app/settings/general/theme.spec.ts
+++ server/tests/app/settings/general/theme.spec.ts
@@ -1,39 +1,10 @@
 import { test, expect } from '@playwright/test'
+import { Color, colors } from '../../utils'
 
 test.use({ storageState: 'server/tests/data/session.json' })
 test.beforeEach(async ({ page }) => {
   await page.goto('/settings')
 })
-
-type Color = {
-  name: string
-  hex: string
-  rgb: string
-}
-
-const colors: Color[] = [
-  { name: 'red', hex: '#f44336', rgb: '244, 67, 54' },
-  { name: 'pink', hex: '#e91e63', rgb: '233, 30, 99' },
-  { name: 'purple', hex: '#9c27b0', rgb: '156, 39, 176' },
-  { name: 'deep purple', hex: '#673ab7', rgb: '103, 58, 183' },
-  { name: 'indigo', hex: '#3f51b5', rgb: '63, 81, 181' },
-  { name: 'blue', hex: '#2196f3', rgb: '33, 150, 243' },
-  { name: 'light blue', hex: '#03a9f4', rgb: '3, 169, 244' },
-  { name: 'cyan', hex: '#00bcd4', rgb: '0, 188, 212' },
-  { name: 'teal', hex: '#009688', rgb: '0, 150, 136' },
-  { name: 'green', hex: '#4caf50', rgb: '76, 175, 80' },
-  { name: 'light green', hex: '#8bc34a', rgb: '139, 195, 74' },
-  { name: 'lime', hex: '#cddc39', rgb: '205, 220, 57' },
-  { name: 'yellow', hex: '#ffeb3b', rgb: '255, 235, 59' },
-  { name: 'amber', hex: '#ffc107', rgb: '255, 193, 7' },
-  { name: 'orange', hex: '#ff9800', rgb: '255, 152, 0' },
-  { name: 'deep orange', hex: '#ff5722', rgb: '255, 87, 34' },
-  { name: 'brown', hex: '#795548', rgb: '121, 85, 72' },
-  { name: 'grey', hex: '#9e9e9e', rgb: '158, 158, 158' },
-  { name: 'blue grey', hex: '#607d8b', rgb: '96, 125, 139' },
-  { name: 'white', hex: '#fff', rgb: '255, 255, 255' },
-  { name: 'black', hex: '#000', rgb: '0, 0, 0' }
-]
 
 test('Dark Mode setting', async ({ page }) => {
   await expect(page.getByLabel('Dark Mode', { exact: true })).not.toBeChecked()
@@ -96,23 +67,24 @@ test.describe('Primary color theme setting', () => {
     await expect(
       page
         .locator('div')
-        .filter({ hasText: /^Backups: --$/ })
+        .filter({ hasText: /^Backups: 29$/ })
         .nth(1)
     ).toHaveCSS('color', `rgb(${indigo.rgb})`)
   })
 
-  colors.forEach((color, index) => {
-    test(`Pick ${color.name} as primary color`, async ({ page }) => {
+  test('Pick colors', async ({ page }) => {
+    for (let i = 0; i < colors.length; i++) {
+      const color = colors[i]
       await expect(
         page
           .locator(
-            `.css-grxim5-themePicker > div > div:nth-child(2) > div > div:nth-child(${index + 1}) > .MuiButtonBase-root`
+            `.css-grxim5-themePicker > div > div:nth-child(2) > div > div:nth-child(${i + 1}) > .MuiButtonBase-root`
           )
           .first()
       ).toHaveAttribute('value', color.hex)
       await page
         .locator(
-          `.css-grxim5-themePicker > div > div:nth-child(2) > div > div:nth-child(${index + 1}) > .MuiButtonBase-root`
+          `.css-grxim5-themePicker > div > div:nth-child(2) > div > div:nth-child(${i + 1}) > .MuiButtonBase-root`
         )
         .first()
         .click()
@@ -135,10 +107,10 @@ test.describe('Primary color theme setting', () => {
       await expect(
         page
           .locator('div')
-          .filter({ hasText: /^Backups: --$/ })
+          .filter({ hasText: /^Backups: 29$/ })
           .nth(1)
       ).toHaveCSS('color', `rgb(${color.rgb})`)
-    })
+    }
   })
 })
 
@@ -168,23 +140,24 @@ test.describe('Secondary color theme setting', () => {
     await expect(
       page
         .locator('div')
-        .filter({ hasText: /^Latest: --$/ })
+        .filter({ hasText: /^Latest: 10\/18\/2024, 12:00:00 AM \(200 KB\)$/ })
         .nth(1)
     ).toHaveCSS('color', `rgb(${pink.rgb})`)
   })
 
-  colors.forEach((color, index) => {
-    test(`Pick ${color.name} as secondary color`, async ({ page }) => {
+  test('Pick colors', async ({ page }) => {
+    for (let i = 0; i < colors.length; i++) {
+      const color = colors[i]
       await expect(
         page
           .locator(
-            `.MuiCardContent-root > div:nth-child(3) > div > div:nth-child(2) > div > div:nth-child(${index + 1}) > .MuiButtonBase-root`
+            `.MuiCardContent-root > div:nth-child(3) > div > div:nth-child(2) > div > div:nth-child(${i + 1}) > .MuiButtonBase-root`
           )
           .first()
       ).toHaveAttribute('value', color.hex)
       await page
         .locator(
-          `.MuiCardContent-root > div:nth-child(3) > div > div:nth-child(2) > div > div:nth-child(${index + 1}) > .MuiButtonBase-root`
+          `.MuiCardContent-root > div:nth-child(3) > div > div:nth-child(2) > div > div:nth-child(${i + 1}) > .MuiButtonBase-root`
         )
         .first()
         .click()
@@ -203,9 +176,9 @@ test.describe('Secondary color theme setting', () => {
       await expect(
         page
           .locator('div')
-          .filter({ hasText: /^Latest: --$/ })
+          .filter({ hasText: /^Latest: 10\/18\/2024, 12:00:00 AM \(200 KB\)$/ })
           .nth(1)
       ).toHaveCSS('color', `rgb(${color.rgb})`)
-    })
+    }
   })
 })
--- server/tests/app/utils.ts
+++ server/tests/app/utils.ts
@@ -1,5 +1,35 @@
 import { Page, Locator } from '@playwright/test'
 
+export type Color = {
+  name: string
+  hex: string
+  rgb: string
+}
+
+export const colors: Color[] = [
+  { name: 'red', hex: '#f44336', rgb: '244, 67, 54' },
+  { name: 'pink', hex: '#e91e63', rgb: '233, 30, 99' },
+  { name: 'purple', hex: '#9c27b0', rgb: '156, 39, 176' },
+  { name: 'deep purple', hex: '#673ab7', rgb: '103, 58, 183' },
+  { name: 'indigo', hex: '#3f51b5', rgb: '63, 81, 181' },
+  { name: 'blue', hex: '#2196f3', rgb: '33, 150, 243' },
+  { name: 'light blue', hex: '#03a9f4', rgb: '3, 169, 244' },
+  { name: 'cyan', hex: '#00bcd4', rgb: '0, 188, 212' },
+  { name: 'teal', hex: '#009688', rgb: '0, 150, 136' },
+  { name: 'green', hex: '#4caf50', rgb: '76, 175, 80' },
+  { name: 'light green', hex: '#8bc34a', rgb: '139, 195, 74' },
+  { name: 'lime', hex: '#cddc39', rgb: '205, 220, 57' },
+  { name: 'yellow', hex: '#ffeb3b', rgb: '255, 235, 59' },
+  { name: 'amber', hex: '#ffc107', rgb: '255, 193, 7' },
+  { name: 'orange', hex: '#ff9800', rgb: '255, 152, 0' },
+  { name: 'deep orange', hex: '#ff5722', rgb: '255, 87, 34' },
+  { name: 'brown', hex: '#795548', rgb: '121, 85, 72' },
+  { name: 'grey', hex: '#9e9e9e', rgb: '158, 158, 158' },
+  { name: 'blue grey', hex: '#607d8b', rgb: '96, 125, 139' },
+  { name: 'white', hex: '#fff', rgb: '255, 255, 255' },
+  { name: 'black', hex: '#000', rgb: '0, 0, 0' }
+]
+
 export async function changeSlider(
   page: Page,
   thumb: Locator,
