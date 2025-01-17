ef96fc2f68c120833f0e9d29e5ab70109c590cb1
regtemp8 <regulartemporary8@proton.me>
2025-01-17 10:45:11 +0200

    move getFileGroup

--- server/src/utils.ts
+++ server/src/utils.ts
@@ -1,3 +1,4 @@
+import { getSourceType, ST } from 'flipflip-common'
 import path from 'path'
 export const isMacOSX = process.platform === 'darwin'
 
@@ -34,7 +35,7 @@ export function getLogsDir() {
   return path.resolve(getSaveDir(), 'logs')
 }
 
-export function getFileName(url: string, extension = true) {
+export function getFileName(url: string, extension = true): string {
   let sep
   if (/^(https?:\/\/)|(file:\/\/)/g.exec(url) != null) {
     sep = '/'
@@ -49,4 +50,179 @@ export function getFileName(url: string, extension = true) {
     url = url.substring(0, url.lastIndexOf('.'))
   }
   return url
+}
+
+export function getFileGroup(url: string): string {
+  let sep;
+  switch (getSourceType(url)) {
+    case ST.tumblr:
+      let tumblrID = url.replace(/https?:\/\//, "");
+      tumblrID = tumblrID.replace(/\.tumblr\.com\/?/, "");
+      return tumblrID;
+    case ST.reddit:
+      let redditID = url;
+      if (redditID.endsWith("/")) redditID = redditID.slice(0, url.lastIndexOf("/"));
+      if (redditID.endsWith("/saved")) redditID = redditID.replace("/saved", "");
+      redditID = redditID.substring(redditID.lastIndexOf("/") + 1);
+      return redditID;
+    case ST.redgifs:
+      let redgifID = '';
+      if (url.includes("/browse?")) {
+        let redgifRegex = /^https?:\/\/(?:www\.)?redgifs\.com\/browse\?.*tags=([^&]*)/.exec(url);
+        return redgifRegex != null ? redgifRegex[1] : "all";
+      } else  if (url.includes("/users/")) {
+        redgifID = url.replace(/^https?:\/\/(www\.)?redgifs\.com\/users\//, "");
+        if (redgifID.includes("/")) {
+          redgifID = redgifID.substring(0, redgifID.indexOf("/"));
+        }
+      }
+      return redgifID;
+    case ST.imagefap:
+      let imagefapID = url.replace(/https?:\/\/www.imagefap.com\//, "");
+      imagefapID = imagefapID.replace(/pictures\//, "");
+      imagefapID = imagefapID.replace(/organizer\//, "");
+      imagefapID = imagefapID.replace(/video\.php\?vid=/, "");
+      imagefapID = imagefapID.split("/")[0];
+      return imagefapID;
+    case ST.sexcom:
+      let sexcomID = url.replace(/https?:\/\/www.sex.com\//, "");
+      sexcomID = sexcomID.replace(/user\//, "");
+      sexcomID = sexcomID.split("?")[0];
+      if (sexcomID.endsWith("/")) {
+        sexcomID = sexcomID.substring(0, sexcomID.length - 1);
+      }
+      return sexcomID;
+    case ST.imgur:
+      let imgurID = url.replace(/https?:\/\/imgur.com\//, "");
+      imgurID = imgurID.replace(/a\//, "");
+      return imgurID;
+    case ST.twitter:
+      let twitterID = url.replace(/https?:\/\/twitter.com\//, "");
+      if (twitterID.includes("?")) {
+        twitterID = twitterID.substring(0, twitterID.indexOf("?"));
+      }
+      if (twitterID.endsWith("/")) {
+        twitterID = twitterID.substring(0, twitterID.length - 1);
+      }
+      return twitterID;
+    case ST.deviantart:
+      let authorID = url.replace(/https?:\/\/www.deviantart.com\//, "");
+      if (authorID.includes("/")) {
+        authorID = authorID.substring(0, authorID.indexOf("/"));
+      }
+      return authorID;
+    case ST.instagram:
+      let instagramID = url.replace(/https?:\/\/www.instagram.com\//, "");
+      if (instagramID.includes("/")) {
+        instagramID = instagramID.substring(0, instagramID.indexOf("/"));
+      }
+      return instagramID;
+    case ST.e621:
+      const hostRegexE621 = /^https?:\/\/(?:www\.)?([^.]*)\./g;
+      const hostE621 =  hostRegexE621.exec(url)![1];
+      let E621ID = "";
+      if (url.includes("/pools/")) {
+        E621ID = "pool" + url.substring(url.lastIndexOf("/"));
+      } else {
+        const tagRegex = /[?&]tags=(.*)&?/g;
+        let tags;
+        if ((tags = tagRegex.exec(url)) !== null) {
+          E621ID = tags[1];
+        }
+        if (E621ID.endsWith("+")) {
+          E621ID = E621ID.substring(0, E621ID.length - 1);
+        }
+      }
+      return hostE621 + "/" + decodeURIComponent(E621ID);
+    case ST.luscious:
+      let albumID = url.replace(/^https?:\/\/(www\.|members\.)?luscious\.net\/(albums|users)\//, "");
+      if (albumID.includes("/")) {
+        albumID = albumID.substring(0, albumID.indexOf("/"));
+      }
+      return albumID;
+    case ST.danbooru:
+    case ST.gelbooru1:
+    case ST.gelbooru2:
+      const hostRegex = /^https?:\/\/(?:www\.)?([^.]*)\./g;
+      const host =  hostRegex.exec(url)![1];
+      let danbooruID = "";
+      if (url.includes("/pools/")) {
+        danbooruID = "pools/" + url.substring(url.lastIndexOf("/"));
+      } else if (url.includes("/favorite_groups/")) {
+        danbooruID = "favorite_groups/" + url.substring(url.lastIndexOf("/"));
+      } else {
+        const tagRegex = /[?&]tags=(.*)&?/g;
+        let tags;
+        if ((tags = tagRegex.exec(url)) !== null) {
+          danbooruID = tags[1];
+        }
+        const titleRegex = /[?&]title=(.*)&?/g;
+        let title;
+        if ((title = titleRegex.exec(url)) !== null) {
+          if (tags == null) {
+            danbooruID = ""
+          } else if (!danbooruID.endsWith("+")) {
+            danbooruID += "+";
+          }
+          danbooruID += title[1];
+        }
+        if (danbooruID.endsWith("+")) {
+          danbooruID = danbooruID.substring(0, danbooruID.length - 1);
+        }
+      }
+      return host + "/" + decodeURIComponent(danbooruID);
+    case ST.ehentai:
+      const galleryRegex = /^https?:\/\/(?:www\.)?e-hentai\.org\/g\/([^\/]*)/g;
+      const gallery = galleryRegex.exec(url);
+      return gallery![1];
+    case ST.list:
+      if (/^https?:\/\//g.exec(url) != null) {
+        sep = "/"
+      } else {
+        sep = path.sep;
+      }
+      return url.substring(url.lastIndexOf(sep) + 1).replace(".txt", "");
+    case ST.local:
+      if (url.endsWith(path.sep)) {
+        url = url.substring(0, url.length - 1);
+        return url.substring(url.lastIndexOf(path.sep)+1);
+      } else {
+        return url.substring(url.lastIndexOf(path.sep)+1);
+      }
+    case ST.video:
+    case ST.playlist:
+    case ST.nimja:
+      if (/^https?:\/\//g.exec(url) != null) {
+        sep = "/"
+      } else {
+        sep = path.sep;
+      }
+      let name = url.substring(0, url.lastIndexOf(sep));
+      return name.substring(name.lastIndexOf(sep)+1);
+    case ST.bdsmlr:
+      let bdsmlrID = url.replace(/https?:\/\//, "");
+      bdsmlrID = bdsmlrID.replace(/\/rss/, "");
+      bdsmlrID = bdsmlrID.replace(/\.bdsmlr\.com\/?/, "");
+      return bdsmlrID;
+    case ST.hydrus:
+      const tagsRegex = /tags=([^&]*)&?.*$/.exec(url);
+      if (tagsRegex == null) return "hydrus";
+      let tags = tagsRegex[1];
+      if (!tags.startsWith("[")) {
+        tags = decodeURIComponent(tags);
+      }
+      tags = tags.substring(1, tags.length - 1);
+      tags = tags.replace(/"/g, "");
+      return tags;
+    case ST.piwigo:
+      const catRegex = /cat_id\[]=(\d*)/.exec(url);
+      if (catRegex != null) return catRegex[1];
+
+      const tagRegex = /tag_id\[]=(\d*)/.exec(url);
+      if (tagRegex != null) return tagRegex[1];
+
+      return "piwigo";
+    default:
+      return ''
+  }
 }
