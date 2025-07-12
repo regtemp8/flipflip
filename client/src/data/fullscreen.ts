495c846e220775fc6d660f812e0eb113c5015497
regtemp8 <regulartemporary8@proton.me>
2025-07-12 20:53:16 +0300

    Add fulscreen util functions

---
+++ client/src/data/fullscreen.ts
@@ -0,0 +1,44 @@
+import fscreen from "fscreen"
+
+function openFullScreen() {
+  if (fscreen.fullscreenEnabled) {
+    const elem = document.documentElement as any
+    fscreen.requestFullscreen(elem)
+  }
+}
+
+function closeFullScreen() {
+  if (fscreen.fullscreenEnabled) {
+    fscreen.exitFullscreen()
+  }
+}
+
+export function toggleFullScreen(): boolean | undefined {
+  if (!fscreen.fullscreenEnabled) {
+    return undefined
+  }
+
+  const isFullScreen = fscreen.fullscreenElement != null
+  if (isFullScreen) {
+    closeFullScreen()
+  } else {
+    openFullScreen()
+  }
+
+  return isFullScreen
+}
+
+export function setFullScreen(fullScreen: boolean): void {
+  if (!fscreen.fullscreenEnabled) {
+    return
+  }
+
+  const isFullScreen = fscreen.fullscreenElement != null
+  if (fullScreen === isFullScreen) {
+    return
+  } else if (fullScreen) {
+    openFullScreen()
+  } else {
+    closeFullScreen()
+  }
+}
