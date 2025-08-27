6aaab99c3c3353b033efccc29e04b29e0af063d2
regtemp8 <regulartemporary8@proton.me>
2025-08-27 13:11:20 +0300

    Improve build script

--- pkg.sh
+++ pkg.sh
@@ -1,4 +1,6 @@
 #/bin/bash
+set -e # exit on error (non-zero exit code)
+set -x # print commands
 
 export SERVER_PORT=5050
 export FF_PORT=5050
@@ -6,18 +8,21 @@ export NODE_ENV=production
 
 echo 'Building common'
 cd ./common
+rm -rf ./build
 yarn install --immutable
 yarn build:main
 yarn build:module
 
 echo 'Building client'
 cd ../client
+rm -rf ./dist
 yarn install --immutable
 yarn build
 
 echo 'Building server'
 cd ../server
 rm -rf ./bin 
+rm -rf ./dist
 yarn install --immutable
 yarn prod
 
