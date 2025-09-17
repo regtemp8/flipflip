ac0c18b2b0865e0a825be12b7d51cceec4ee7a6a
regtemp8 <regulartemporary8@proton.me>
2025-09-17 12:33:36 +0300

    Test single scene playlist options

--- server/tests/app/scenes/scene-detail.spec.ts
+++ server/tests/app/scenes/scene-detail.spec.ts
@@ -3,29 +3,65 @@ import { test, expect } from '@playwright/test'
 test('Add scene', async ({ page }) => {
   await page.goto('/')
   await page.getByTestId('AddIcon').click()
+  await expect(page.getByRole('dialog')).not.toBeVisible()
+  await page
+    .getByLabel('Add Display', { exact: true })
+    .getByRole('button')
+    .click()
+
+  await expect(page).toHaveURL('/displays/1')
+  await page.goto('/')
+  await page.getByTestId('AddIcon').click()
   await page.getByRole('button', { name: 'Add Scene', exact: true }).click()
-  await expect(page).toHaveURL('/scenes/2')
+  await expect(page).toHaveURL('/scenes/1')
+  await expect(page.locator('#title')).toBeFocused()
+  await expect(page.locator('#title')).toHaveValue('New scene')
+
+  await page.goto('/displays/1')
+  await expect(page).toHaveURL('/displays/1')
+  await page
+    .getByRole('combobox', { name: 'Scene Playlist', exact: true })
+    .click()
+  await expect(
+    page.getByRole('option', { name: 'New scene', exact: true })
+  ).toBeVisible()
+
   const responsePromise = page.waitForResponse((res) => {
     const request = res.request()
     return (
-      new URL(request.url()).pathname === '/api/scenes/2' &&
+      new URL(request.url()).pathname === '/api/scenes/1' &&
       request.method() === 'PATCH' &&
       request.postDataJSON()?.name === 'My scene' &&
       res.status() === 204
     )
   })
+  await page.goto('/scenes/1')
+  await page.getByRole('heading', { name: 'New scene', exact: true }).dblclick()
   await expect(page.locator('#title')).toBeFocused()
-  await expect(page.locator('#title')).toHaveValue('New scene')
   await page.locator('#title').fill('My scene')
   await page.locator('#title').press('Enter')
   await expect(page.getByRole('heading')).toHaveText('My scene')
   await responsePromise
-  await page.getByRole('button', { name: 'Back' }).click()
+  await page.goto('/')
   await expect(page).toHaveURL('/')
+  await page.locator('#vertical-tab-4').click()
+  await expect(page).toHaveURL('/playlists')
+  await expect(
+    page.getByRole('button', { name: 'My scene', exact: true })
+  ).not.toBeVisible()
   await page.locator('#vertical-tab-0').click()
   await expect(page).toHaveURL('/scenes')
   await page.getByRole('button', { name: 'My scene', exact: true }).click()
-  await expect(page).toHaveURL('/scenes/2')
+  await expect(page).toHaveURL('/scenes/1')
+
+  await page.goto('/displays/1')
+  await expect(page).toHaveURL('/displays/1')
+  await page
+    .getByRole('combobox', { name: 'Scene Playlist', exact: true })
+    .click()
+  await expect(
+    page.getByRole('option', { name: 'My scene', exact: true })
+  ).toBeVisible()
 })
 
 // TODO add scene tests
@@ -33,8 +69,8 @@ test('Add scene', async ({ page }) => {
 test('Clone scene', async ({ page }) => {
   await page.goto('/')
   await expect(page).toHaveURL('/')
-  await page.goto('/scenes/2')
-  await expect(page).toHaveURL('/scenes/2')
+  await page.goto('/scenes/1')
+  await expect(page).toHaveURL('/scenes/1')
 
   await page.getByRole('button', { name: 'Clone Scene', exact: true }).hover()
   await expect(
@@ -42,13 +78,13 @@ test('Clone scene', async ({ page }) => {
   ).toBeVisible()
   await page.getByRole('button', { name: 'Clone Scene', exact: true }).click()
 
-  await expect(page).toHaveURL('/scenes/3')
+  await expect(page).toHaveURL('/scenes/2')
   await expect(page.getByRole('alert')).toHaveText('Clone successful!')
 
   const responsePromise = page.waitForResponse((res) => {
     const request = res.request()
     return (
-      new URL(request.url()).pathname === '/api/scenes/3' &&
+      new URL(request.url()).pathname === '/api/scenes/2' &&
       request.method() === 'PATCH' &&
       request.postDataJSON()?.name === 'Clone scene' &&
       res.status() === 204
@@ -61,7 +97,17 @@ test('Clone scene', async ({ page }) => {
   await expect(page.getByRole('heading')).toHaveText('Clone scene')
   await responsePromise
   await page.getByRole('button', { name: 'Back' }).click()
-  await expect(page).toHaveURL('/scenes/2')
+  await expect(page).toHaveURL('/scenes/1')
+
+  await page.goto('/playlists')
+  await expect(page).toHaveURL('/playlists')
+  await expect(
+    page.getByRole('button', { name: 'My scene', exact: true })
+  ).not.toBeVisible()
+  await expect(
+    page.getByRole('button', { name: 'Clone scene', exact: true })
+  ).not.toBeVisible()
+
   await page.goto('/scenes')
   await expect(page).toHaveURL('/scenes')
   await expect(
@@ -71,7 +117,19 @@ test('Clone scene', async ({ page }) => {
     page.getByRole('button', { name: 'Clone scene', exact: true })
   ).toBeVisible()
   await page.getByRole('button', { name: 'Clone scene', exact: true }).click()
-  await expect(page).toHaveURL('/scenes/3')
+  await expect(page).toHaveURL('/scenes/2')
+
+  await page.goto('/displays/1')
+  await expect(page).toHaveURL('/displays/1')
+  await page
+    .getByRole('combobox', { name: 'Scene Playlist', exact: true })
+    .click()
+  await expect(
+    page.getByRole('option', { name: 'My scene', exact: true })
+  ).toBeVisible()
+  await expect(
+    page.getByRole('option', { name: 'Clone scene', exact: true })
+  ).toBeVisible()
 })
 
 test('Delete cloned scene', async ({ page }) => {
@@ -84,7 +142,7 @@ test('Delete cloned scene', async ({ page }) => {
     page.getByRole('button', { name: 'Clone scene', exact: true })
   ).toBeVisible()
   await page.getByRole('button', { name: 'Clone scene', exact: true }).click()
-  await expect(page).toHaveURL('/scenes/3')
+  await expect(page).toHaveURL('/scenes/2')
 
   await page.getByRole('button', { name: 'Delete Scene', exact: true }).hover()
   await expect(
@@ -114,7 +172,7 @@ test('Delete cloned scene', async ({ page }) => {
   ).not.toBeDisabled()
 
   await page.getByRole('button', { name: 'Cancel', exact: true }).click()
-  await expect(page).toHaveURL('/scenes/3')
+  await expect(page).toHaveURL('/scenes/2')
 
   await page.getByRole('button', { name: 'Delete Scene', exact: true }).click()
   await expect(page.getByRole('dialog')).toBeVisible()
@@ -126,13 +184,25 @@ test('Delete cloned scene', async ({ page }) => {
   await expect(
     page.getByRole('button', { name: 'Clone scene', exact: true })
   ).not.toBeVisible()
+
+  await page.goto('/displays/1')
+  await expect(page).toHaveURL('/displays/1')
+  await page
+    .getByRole('combobox', { name: 'Scene Playlist', exact: true })
+    .click()
+  await expect(
+    page.getByRole('option', { name: 'My scene', exact: true })
+  ).toBeVisible()
+  await expect(
+    page.getByRole('option', { name: 'Clone scene', exact: true })
+  ).not.toBeVisible()
 })
 
 test('Delete scene', async ({ page }) => {
   await page.goto('/')
   await expect(page).toHaveURL('/')
-  await page.goto('/scenes/2')
-  await expect(page).toHaveURL('/scenes/2')
+  await page.goto('/scenes/1')
+  await expect(page).toHaveURL('/scenes/1')
 
   await page.getByRole('button', { name: 'Delete Scene', exact: true }).hover()
   await expect(
@@ -162,10 +232,22 @@ test('Delete scene', async ({ page }) => {
   ).not.toBeDisabled()
 
   await page.getByRole('button', { name: 'Cancel', exact: true }).click()
-  await expect(page).toHaveURL('/scenes/2')
+  await expect(page).toHaveURL('/scenes/1')
 
   await page.getByRole('button', { name: 'Delete Scene', exact: true }).click()
   await expect(page.getByRole('dialog')).toBeVisible()
   await page.getByRole('button', { name: 'OK', exact: true }).click()
   await expect(page).toHaveURL('/')
+
+  await page.goto('/displays/1')
+  await expect(page).toHaveURL('/displays/1')
+  await page
+    .getByRole('combobox', { name: 'Scene Playlist', exact: true })
+    .click()
+  await expect(
+    page.getByRole('option', { name: 'My scene', exact: true })
+  ).not.toBeVisible()
+  await expect(
+    page.getByRole('option', { name: 'Clone scene', exact: true })
+  ).not.toBeVisible()
 })
